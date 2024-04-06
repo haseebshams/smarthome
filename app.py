1a6b3d585671e960ee9168ea6a8fc44147943a53
Haseeb Shams <56117409+haseebshams@users.noreply.github.com>
2024-04-06 03:04:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 import cv2
 import requests
 import numpy as np
-import json
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Azure Custom Vision endpoint and prediction key
 ENDPOINT = "https://asedetection-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/cb499bb3-445c-4295-8689-d8f584dea194/classify/iterations/Iteration3/image"
 PREDICTION_KEY = "470836658fc844cb9f9416498467580d"
 PROJECT_ID = "cb499bb3-445c-4295-8689-d8f584dea194"
+
 
 # Function to detect gender and age using Azure Custom Vision
 def detect_gender_age(image):
@@ -33,30 +35,38 @@ def detect_gender_age(image):
         print(f"An error occurred: {e}")
         return 'Unknown', 'Unknown'
 
+
 # Home route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route to get gender and age predictions
+
 @app.route('/predict')
 def predict():
-    # Capture frame from webcam
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
+    try:
+        url = "http://192.168.2.219:8080/video"
+        cap = cv2.VideoCapture(url)
+        ret, frame = cap.read()
+        cap.release()
 
-    # Convert the frame to binary
-    _, buffer = cv2.imencode('.jpg', frame)
-    image_data = buffer.tobytes()
+        if not ret:
+            raise Exception("Failed to retrieve frame from webcam")
 
-    # Detect gender and age
-    gender, age = detect_gender_age(image_data)
+        # Convert the frame to binary
+        _, image = cv2.imencode('.jpg', frame)
+        image_data = image.tobytes()
 
-    # Release the camera
-    cap.release()
+        # Detect gender and age
+        gender, age = detect_gender_age(image_data)
 
-    # Return JSON response
-    return jsonify({"gender": gender, "age": age})
+        # Return JSON response
+        return jsonify({"gender": gender, "age": age})
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({"error": "Failed to process the request"}), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
